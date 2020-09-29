0bd6658d0244c971d1636bf8f5eb6c577af80b5c
Lerdor <marshal604@gmail.com>
2020-09-29 21:52:02 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/debt-assistant/precache-manifest.57a634de6430927db355b4fd57a3c31d.js"
+  "/debt-assistant/precache-manifest.82e886e5165de58609c54af527bcb7d9.js"
 );
 
 self.addEventListener('message', (event) => {
