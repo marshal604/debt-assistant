5e4eb7a4060b907af2e103e4d667d9295d09496e
marshal604 <marshal604@gmail.com>
2020-10-01 15:55:52 +0000

    Deploying to gh-pages from  @ 6647054ad8127e389b6e8c8589dbd0c162b77cd3 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/debt-assistant/precache-manifest.e58fcdd9b25e2dd35f2c11382dcd8834.js"
+  "/debt-assistant/precache-manifest.a908dcdaffde41bbf4971fca2a1cbd65.js"
 );
 
 self.addEventListener('message', (event) => {
